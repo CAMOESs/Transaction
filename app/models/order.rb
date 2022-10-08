b44140c60640989f63dd084959a8e2d4c1fe382a
lahanatou <nouryous30@gmail.com>
2022-10-08 17:00:44 +0100

    First commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,10 +5,11 @@ class Order < ApplicationRecord
   accepts_nested_attributes_for :ordered_lists
 
   def update_total_quantity
-    self.ordered_lists.each do |line_item|
-      item = Item.find_by(id: line_item.item_id)
-      item.total_quantity += line_item.quantity
-      item.save!
+    ActiveRecord::Base.transaction do
+      @order = current_user.orders.lock.build(order_params)
+      @order = current_user.orders.build(order_params)
+      @order.save
+      @order.update_total_quantity
+    end
     end
   end
-end
